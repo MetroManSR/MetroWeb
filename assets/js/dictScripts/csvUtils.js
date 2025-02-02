0ce01de4fcdd20d0aa428d6f0266273b0339483f
MetroMan <services@metroman.me>
2025-02-02 19:45:29 -0300

    Update csvUtils.js

--- assets/js/dictScripts/csvUtils.js
+++ assets/js/dictScripts/csvUtils.js
@@ -51,7 +51,7 @@ export async function cleanData(data, type, allRows) {
             cleanedRow.partofspeech = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col2 ? row.col2.trim() : '') : row.col2 ? row.col2.trim() : ''); // Part of Speech for words
             cleanedRow.meta = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col3 ? row.col3.trim() : '') : row.col3 ? row.col3.trim() : ''); // Meta for words
             cleanedRow.notes = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col4 ? row.col4.trim() : '') : row.col4 ? row.col4.trim() : ''); // Notes for words
-            cleanedRow.revision = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col5 ? row.col5.trim() : '') : row.col5 ? row.col5.trim() : ''); // Notes for words
+            cleanedRow.revision = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col5 ? row.col6.trim() : '') : row.col6 ? row.col6.trim() : ''); // Notes for words
 
             let morphData = row.col5 ? row.col5.trim().split(',').map(item => item.trim()) : [];
 
@@ -68,7 +68,7 @@ export async function cleanData(data, type, allRows) {
         } else if (type === 'root') {
             cleanedRow.title = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col1 ? row.col1.trim() : '') : row.col1 ? row.col1.trim() : ''); // Word title for roots
             cleanedRow.meta = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col2 ? row.col2.trim() : '') : row.col2 ? row.col2.trim() : ''); // Meta for roots
-            cleanedRow.revision = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col3 ? row.col3.trim() : '') : row.col3 ? row.col3.trim() : ''); // Notes for words
+            cleanedRow.revision = sanitizeHTML(idsNeedingFixing.includes(index) ? fixEncoding(row.col3 ? row.col4.trim() : '') : row.col4 ? row.col4.trim() : ''); // Notes for words
 
             const notesAndEtimology = row.col3 ? row.col3.trim() : '';
             if (notesAndEtimology.includes('|')) {
