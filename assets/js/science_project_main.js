53919f0e363ef92c4b8f012295deedcec305dc86
MetroMan <services@metroman.me>
2025-02-08 14:00:12 -0300

    Update science_project_main.js

--- assets/js/science_project_main.js
+++ assets/js/science_project_main.js
@@ -1,6 +1,6 @@
 // Import all other scripts
 import { checkPassword } from './scientific_project/password.js';
-import { displaySellers, randomizeSellerStates, sellers } from './scientific_project/seller.js';
+import { displaySellers, randomizeSellerStates, sellers } from './scientific_project/sellers.js';
 import { simulateTicket, showPopup, closePopup, showSellerButtons } from './scientific_project/ticket.js';
 import { startTimers } from './scientific_project/timer.js';
 
